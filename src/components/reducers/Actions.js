f2bab390a93c837c6698602059ed23f6f0c684b1
karenne29 <56751691+karenne29@users.noreply.github.com>
2022-10-06 20:54:13 -0500

    Create Actions.js

---
+++ src/components/reducers/Actions.js
@@ -0,0 +1,15 @@
+const actions = {
+	// Acción que agrega un nuevo platillo al carrito
+	AGREGAR_PRENDA: "AGREGAR_PRENDA",
+
+	// Acción que actualice un platillo existente en el carrito
+	ACTUALIZAR_PRENDA: "ACTUALIZAR_PRENDA",
+
+	// Acción que abre el modal del carrito
+	OPEN_MODAL: "OPEN_MODAL",
+
+	// Acción que cierra el modal del carrito
+	CLOSE_MODAL: "CLOSE_MODAL",
+};
+
+export default actions;
