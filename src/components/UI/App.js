bdbe1029882798724f7ef2bd9f03ad1d8289e85b
Tomas Emiliano Cienega Ortega <tomascienega0206@gmail.com>
2022-10-08 18:14:24 -0500

    Codigo nuevo implementado

--- src/components/UI/App.js
+++ src/components/UI/App.js
@@ -3,38 +3,35 @@ import Producto from '../../pages/Productos';
 import Cart from '../../components/pedidos/Cart'
 import React, { useState } from 'react';
 import cx from 'classnames';
-import globalStyles from '../../assets/global-styles/bootstrap.min.module.css';
 import reducer from "../reducers/Reducer";
 import initialState from "../reducers/InitialState";
 import { useReducer } from "react";
 import StateContext from "../context/state";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import InicioView from '../inicio/InicioView';
+import NotFoundView from "../NotFoundView/NotFoundView";
+import CheckOut from '../btnCheckOut/CheckOut';
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  
   let [cantidad, setCantidad] = useState(0);
   // const agregarCarrito = (event) => {
   //   setCantidad((prevState) => cantidad += parseInt(event))
-    
   // }
-
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
   return (
-
     <StateContext.Provider value={{ state, dispatch }}>
-
-      <Cart/>
-      <div className={cx(globalStyles['container-fluid'], globalStyles['my-4'], globalStyles['p-3'])}>
-        <div className={cx(globalStyles.div, globalStyles['row'], globalStyles['text-center'])}>
-          <Producto/>
-        </div>
-      </div>
+      <Router>
+        <Cart />
+            <Routes>
+              <Route path="/" element={<InicioView />} />
+              <Route path="store" element={<Producto />} />
+              <Route path="checkout" element={<CheckOut />} />
+              <Route path="*" element={<NotFoundView />} />
+            </Routes>
+      </Router>
     </StateContext.Provider>
   );
 }
-
 export default App;
--- src/components/modalCart/ModalCart.js
+++ src/components/modalCart/ModalCart.js
@@ -1,54 +1,55 @@
 import Modal from 'react-bootstrap/Modal';
 import ModalContext from '../context/ModalContext';
 import React, { useContext } from 'react';
-import  Button  from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import styles from './ModalCart.module.css'
 import actions from "../reducers/Actions";
 import StateContext from '../context/state';
 import cx from 'classnames';
 import globalStyles from '../../assets/global-styles/bootstrap.min.module.css';
+import { Link } from 'react-router-dom';
 
 
 function ModalCart(props) {
 
 
     const { state, dispatch } = useContext(StateContext);
-	const total = state.cart.reduce((sum, item) => sum + item.quantity, 0);
+    const total = state.cart.reduce((sum, item) => sum + item.quantity, 0);
     const totalCompra = state.cart.reduce((sum, item) => sum + (item.quantity * item.meal.precio), 0);
 
     const showModal = () => {
         dispatch({
-			type: actions.OPEN_MODAL,
-		});
+            type: actions.OPEN_MODAL,
+        });
     }
 
     const hideModal = () => {
         dispatch({
-			type: actions.CLOSE_MODAL,
-		});
+            type: actions.CLOSE_MODAL,
+        });
     }
 
     function increment(id) {
-		dispatch({
-			type: actions.UPDATE_MEAL,
-			payload: { id, quantity: 1 },
-		});
-	}
+        dispatch({
+            type: actions.UPDATE_MEAL,
+            payload: { id, quantity: 1 },
+        });
+    }
 
-	function decrement(id) {
-		dispatch({
-			type: actions.UPDATE_MEAL,
-			payload: { id, quantity: -1 },
-		});
-	}
+    function decrement(id) {
+        dispatch({
+            type: actions.UPDATE_MEAL,
+            payload: { id, quantity: -1 },
+        });
+    }
 
-    function eliminar(id){
+    function eliminar(id) {
 
         dispatch({
 
             type: actions.ELIMINAR_PRENDA,
 
-            payload: {id},
+            payload: { id },
 
         });
 
@@ -56,12 +57,12 @@ function ModalCart(props) {
 
     return (
 
-        
-            <>
+
+        <>
             <button className={styles['btn-not']} onClick={showModal}>
                 <i className="bi bi-bag-check-fill"></i>
                 <span className={styles['mar-lr']}>
-                Carrito
+                    Carrito
                 </span>
                 <span className="badge bg-primary">{total}</span>
             </button>
@@ -74,29 +75,29 @@ function ModalCart(props) {
                     <Modal.Title>🛒 Mi Carrito</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                {state.cart.map((cartItem) => (
-						<li>
-							<section >
-								<article>
-                                <img src={cartItem.meal.url_img} className={cx(styles['card-img-top'])} alt="..."></img>
-									<h2>{cartItem.meal.nombre_comida}</h2>
-									<div>
-										<h4>$ {cartItem.meal.precio * cartItem.quantity}</h4>
-										<figure>
-											<h4>x {cartItem.quantity}</h4>
-										</figure>
-									</div>
-								</article>
-								<article>
-                                   {cartItem.quantity > 1 ?  <Button onClick={() => decrement(cartItem.meal.id)}>-</Button> : <a></a> } 
-									
-									<Button
-										
-										onClick={() => increment(cartItem.meal.id)}
-									>
-										+
-									</Button>
-                                    <Button    
+                    {state.cart.map((cartItem) => (
+                        <li>
+                            <section >
+                                <article>
+                                    <img src={cartItem.meal.url_img} className={cx(styles['card-img-top'])} alt="..."></img>
+                                    <h2>{cartItem.meal.nombre_comida}</h2>
+                                    <div>
+                                        <h4>$ {cartItem.meal.precio * cartItem.quantity}</h4>
+                                        <figure>
+                                            <h4>x {cartItem.quantity}</h4>
+                                        </figure>
+                                    </div>
+                                </article>
+                                <article>
+                                    {cartItem.quantity > 1 ? <Button onClick={() => decrement(cartItem.meal.id)}>-</Button> : <a></a>}
+
+                                    <Button
+
+                                        onClick={() => increment(cartItem.meal.id)}
+                                    >
+                                        +
+                                    </Button>
+                                    <Button
 
                                         onClick={() => eliminar(cartItem.meal.id)}
 
@@ -105,27 +106,29 @@ function ModalCart(props) {
                                         DELETE
 
                                     </Button>
-								</article>
-							</section>
-						</li>
-					))
-                    
+                                </article>
+                            </section>
+                        </li>
+                    ))
+
                     }
-                    
-                    <br/>
+
+                    <br />
                     <h4>Total compra: ${totalCompra}</h4>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="danger" onClick={hideModal}>
                         Continuar Comprando
                     </Button>
-                    <Button variant="primary" onClick={hideModal}>
-                        Ir a Pagar
-                    </Button>
+                    <Link to="/checkout">
+                        <Button variant="primary" onClick={hideModal}>
+                            Ir a Pagar
+                        </Button>
+                    </Link>
                 </Modal.Footer>
             </Modal>
         </>
-        
+
     );
 }
 
