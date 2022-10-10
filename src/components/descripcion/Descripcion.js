cb0b5ecd0cd2c179404ed7704463acc4a908ae44
Fanny Fuentes <233257@ust-global.com>
2022-10-10 10:31:47 -0500

    lals

--- src/components/descripcion/Descripcion.js
+++ src/components/descripcion/Descripcion.js
@@ -2,10 +2,12 @@ import styles from './Descripcion.module.css'
 import cx from 'classnames';
 import globalStyles from '../../assets/global-styles/bootstrap.min.module.css';
 import React, { Fragment, useContext, useEffect, useState } from 'react';
-import img from '../../img/ropa.jpg'
 import { useParams } from 'react-router-dom';
-import StateContext from '../context/state';
 import MenuContext from '../context/menu';
+import actions from "../reducers/Actions";
+import StateContext from '../context/state';
+import Alert from '../alert/AlertDesc'
+import {  toast } from 'react-toastify';
 
 function Descripcion() {
     let fR = 0;
@@ -21,29 +23,48 @@ function Descripcion() {
     }
 
     const { id } = useParams();
-    const { dispatch, state } = useContext(StateContext);
     const BASE_URL = "https://ropa-http-default-rtdb.firebaseio.com/";
     const [producto, setProducto] = useState([]);
-
+    const meals = useContext(MenuContext);
+    const { dispatch } = useContext(StateContext);
 
     useEffect(() => {
 
         const fetchUser = async () => {
-            //const userId = localStorage.getItem("userId");
             const url = `${BASE_URL}/productos.json?orderBy="$key"&equalTo="${id}"`;
             const response = await fetch(url);
 
-            if (!response.ok) throw new Error("Algo salió mal :(");
+            if (!response.ok) throw new Error("Something went wrong :(");
 
             const responseData = await response.json();
             setProducto(Object.values(responseData)[0]);
-            // console.log(precio)
+            
         };
         fetchUser()
+        console.log(producto)
     }, []);
 
-    return (
 
+    function agregarPrenda() {
+        const meal = meals[id];
+        success()
+        dispatch({
+            type: actions.AGREGAR_PRENDA,
+            payload: { meal, quantity: parseInt("1") },
+        });
+        
+    }
+    const success = () => {
+        setTimeout(() => {
+          toast.success('Succesfully added!', {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 2000,
+            className: styles['msj-success']
+          });
+        }, 0);
+      };
+
+    return (
             <Fragment>
                 <div className={cx(globalStyles['container'], styles['bg-otro'])}>
                     <div className={cx(globalStyles['row'])}>
@@ -57,12 +78,12 @@ function Descripcion() {
                         </div>
                         <div className={cx(styles['div-info'], globalStyles['col-md-6'], globalStyles['d-flex'], globalStyles['justify-content-start'])}>
                             <div className={cx(globalStyles['col-md-9'], globalStyles['p-4'], styles['bg-black'], globalStyles['m-3'])}>
-                                <p>
-                                    {producto.descripcion}
+                                <p className={cx(styles["titulo1"])}>
+                                   {producto.descripcion}
                                 </p>
                                 <p className={styles['clasificacion']}>
 
-                                    <input id="radio1" type="radio" name="estrellas" value="5" checked={true} />
+                                    <input id="radio1" type="radio" name="estrellas" value="5" />
                                     <label for="radio1">★</label>
                                     <input id="radio2" type="radio" name="estrellas" value="4" />
                                     <label for="radio2">★</label>
@@ -72,19 +93,43 @@ function Descripcion() {
                                     <label for="radio4">★</label>
                                     <input id="radio5" type="radio" name="estrellas" value="1" />
                                     <label for="radio5">★</label>
-                                    <span className={styles['p-coments']}>(+100 Comentarios)</span>
+                                    <span className={styles['p-coments']}>(+100 comments)</span>
                                 </p>
 
                                 <p className={cx(styles['p-pre'])}>
-                                    $MXN{producto.precio}
+                                    ${producto.precio} MXN
                                 </p>
                                 <p>
-                                    <b>Color</b>: Negro Pasion
+                                    <b>Color</b>: As shown
                                 </p>
+
+                                <p>
+                                    <b>
+                                        Sizes:
+                                    </b>
+                                </p>
+
+                                <div className={cx(globalStyles["btn-group"], globalStyles['mb-3'], styles['btn-g-tal'])} role="group" aria-label="Basic radio toggle button group">
+                                    <input type="radio" className="btn-check btn-talla" name="btnradio" id="btnradio1" autoComplete="off" />
+                                    <label className="btn btn-talla" for="btnradio1">XS</label>
+
+                                    <input type="radio" className="btn-check btn-talla" name="btnradio" id="btnradio2" autoComplete="off" />
+                                    <label className="btn btn-talla" for="btnradio2">S</label>
+
+                                    <input type="radio" className="btn-check btn-talla" name="btnradio" id="btnradio3" autoComplete="off" />
+                                    <label className="btn btn-talla" for="btnradio3">M</label>
+
+                                    <input type="radio" className="btn-check btn-talla" name="btnradio" id="btnradio4" autoComplete="off" />
+                                    <label className="btn btn-talla" for="btnradio4">L</label>
+                                </div>
+
                                 <div className={cx(globalStyles['d-flex'], globalStyles['justify-content-between'])}>
-                                    <button className={cx(globalStyles['btn'], styles['btn-add'])}>
+                                    {/* <button className={cx(globalStyles['btn'], styles['btn-add'])} onClick={agregarPrenda}>
                                         ANADIR AL CARRITO
-                                    </button>
+                                    </button> */}
+                                    <Alert onClick={() => agregarPrenda()}>
+
+                                    </Alert>
                                     <button id='btn-heart' className={cx(styles['btn-not'])} onClick={addFav}>
                                         <i className="fa fa-light fa-heart fa-2x"></i>
                                     </button>
@@ -98,12 +143,12 @@ function Descripcion() {
                                         <div className={cx(globalStyles['col-md-10'])}>
                                             <p>
                                                 <b>
-                                                    Envío gratuito
+                                                    Free shipping
                                                 </b>
                                             </p>
                                             <p className={styles['envio-p']}>
-                                                Envío gratuito estándar para pedidos de más de $MXN99.00
-                                                Entrega estimada el día 23/10/2022 - 25/10/2022.
+                                                Standard free shipping for orders over $99.00 MXN.
+                                                Estimated delivery on 23/10/2022 - 25/10/2022
                                             </p>
                                         </div>
                                     </div>
--- src/components/inicio/InicioView.js
+++ src/components/inicio/InicioView.js
@@ -3,9 +3,15 @@ import cx from 'classnames';
 import globalStyles from '../../assets/global-styles/bootstrap.min.module.css';
 import React, { useContext, useState } from 'react';
 import { Link } from "react-router-dom";
+import img1 from '../../img/img1.jpg'
+import info1 from '../../img/info1.gif'
+import info2 from '../../img/info2.gif'
+import info3 from '../../img/info3.gif'
+import ropa1 from '../../img/ropa1.jpg'
+import ropa2 from '../../img/ropa2.jpg'
+import ropa3 from '../../img/ropa3.jpg'
 // import ModalContext from '../context/ModalContext';
 //import ModalCart from '../modalCart/ModalCart'
-
 function InicioView() {
     return (
         <React.Fragment>
@@ -14,18 +20,20 @@ function InicioView() {
                     <path fill="#FFFFFF" fillOpacity="1" d="M0,96L80,85.3C160,75,320,53,480,85.3C640,117,800,203,960,245.3C1120,288,1280,288,1360,288L1440,288L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
                     </path>
                 </svg> */}
-                <svg width="100%" height="30%" id="svg" data-testid="imag-ini01" viewBox="0 0 1440 700" className={cx(styles['svg'])} xmlns="http://www.w3.org/2000/svg" ><defs>
+                <svg width="100%" height="30%" id="svg" viewBox="0 0 1440 700" className={cx(styles['svg'])} xmlns="http://www.w3.org/2000/svg"><defs>
                 </defs>
                     <path d="M 0,700 C 0,700 0,350 0,350 C 134,397.32142857142856 268,444.64285714285717 403,426 C 538,407.35714285714283 674,322.75 773,281 C 872,239.24999999999997 934,240.3571428571429 1039,259 C 1144,277.6428571428571 1292,313.82142857142856 1440,350 C 1440,350 1440,700 1440,700 Z" stroke="none" strokeWidth="0" fill="#FFFFFF" fillOpacity="1">
                         {/* class="transition-all duration-300 ease-in-out delay-150 path-0"> */}
                     </path>
                 </svg>
-                <div className={styles['div-somos']} data-testid="mono">
+                <div className={styles['div-somos']}>
                     <p className={styles['h2-somos']}>
-                        In HEIN we are committed to offer our clients the best products in clothing wih the highest standards of quality at a reasonable price.
-                        We hope you can enjoy the experience of buying in our new web page.
+                        "In HEIN we are committed to offer our clients the best products in clothing wih the highest standards of quality at a reasonable price.
+                        We hope you can enjoy the experience of buying in our new web page."
                     </p>
-                    <Link to="/store" data-testid="link-ini01" className="btn btn-outline-dark d-grid gap-2 btn-lg">GO SHOPPING</Link>
+
+                    <Link to="/store" className={cx(globalStyles['btn'], styles['btn-dark'])}>🛍 GO SHOPPING 🛍</Link>
+
                     {/* <p className={styles['p-somos']}>
                         Fashion is the armor to survive the reality of everyday life.
                     </p>
@@ -35,6 +43,45 @@ function InicioView() {
                 </div>
             </div>
             <div className={styles['container-somos']}>
+            </div>
+
+            <div className={globalStyles['container']}>
+                <div className={globalStyles['row']}>
+                    <div className={cx(globalStyles['col-md-4'], globalStyles['p-0'])}>
+                        <img src={info1} className={styles['img-1']}></img>
+                    </div>
+                    <div className={cx(globalStyles['col-md-4'], globalStyles['p-0'])}>
+                        <img src={info2} className={styles['img-1']}></img>
+                    </div>
+                    <div className={cx(globalStyles['col-md-4'], globalStyles['p-0'])}>
+                        <img src={info3} className={styles['img-1']}></img>
+                    </div>
+                </div>
+                <div className={globalStyles['row']}>
+                    <div className={cx(globalStyles['col-lg-12'], styles['bg-gray'], globalStyles['p-0'])}>
+                        <img src={img1} className={styles['img-1']}></img>
+                    </div>
+                </div>
+
+                <div className={cx(globalStyles['container'], globalStyles['mt-3'], globalStyles['p-0'], globalStyles['text-center'])}>
+                    <p className={styles['p-subtitle']}>
+                        LA MEJOR CALIDAD Y DISENO LA ENCONTRARAS AQUI
+                    </p>
+                    <div className={cx(globalStyles['row'])}>
+
+                        <div class={cx(globalStyles['col-md-4'])} >
+                            <img src={ropa1} className={cx(styles['img-1'])}></img>
+                        </div>
+                        <div class="col-md-4">
+                            <img src={ropa2} className={styles['img-1']}></img>
+                        </div>
+                        <div class="col-md-4">
+                            <img src={ropa3} className={styles['img-1']}></img>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </React.Fragment>
     );
