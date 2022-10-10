2798bf31aaddf2cc12383f8845ff20e2d7318670
Fanny Fuentes <233257@ust-global.com>
2022-10-10 11:43:44 -0500

    Test final

--- src/components/pedidos/Cart.js
+++ src/components/pedidos/Cart.js
@@ -6,46 +6,26 @@ import React, { useContext, useState } from "react";
 import ModalCart from "../modalCart/ModalCart";
 import logo from "../../img/logo2.png";
 import { Link } from "react-router-dom";
-
 function Cart(props) {
-
   return (
     <React.Fragment>
       <div className={cx(globalStyles['d-flex'], styles['container'])}>
         <div className={cx(globalStyles['container-fluid'], styles['container'])}>
-
           <div className={styles['div-title']}>
             <Link to="/">
-              <img src={logo} height={70} width={180} />
+              <img data-testid="ImageNav" src={logo} height={70} width={180} />
             </Link>
-
-
-    return (
-        <React.Fragment>
-            <div className={cx(globalStyles['container-fluid'], styles['container'])}>
-
-                <div className={styles['div-title']}>
-                    <Link to="/">
-                        <img data-testid="ImageNav" src={logo} height={70} width={180} />
-                    </Link>
-                    <div>
-                        <Link to="/" className={cx(globalStyles["btn"], globalStyles['btn-outline-secondary'], styles['btn-go2'])}>HOME</Link>
-                        <Link to="/store" className={cx(globalStyles["btn"], globalStyles['btn-outline-secondary'], styles['btn-go'])}>GO SHOPPING</Link>
-                    </div>
-                </div>
-
+            <div>
+              <Link to="/" className={cx(globalStyles["btn"], globalStyles['btn-outline-secondary'], styles['btn-go2'])}>HOME</Link>
+              <Link to="/store" className={cx(globalStyles["btn"], globalStyles['btn-outline-secondary'], styles['btn-go'])}>GO SHOPPING</Link>
             </div>
           </div>
-
           <div className={styles["div-cart"]}>
             <ModalCart />
           </div>
         </div>
       </div>
-
     </React.Fragment>
   );
-
 }
-
 export default Cart;
